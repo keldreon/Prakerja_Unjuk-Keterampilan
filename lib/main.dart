74dd68a02cd8d8643900e0a5fffe59c3a295a826
Keldreon <galih91@live.com>
2023-07-01 07:10:53 +0700

    added more custom icon, bottom navbar, but no theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,12 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shop Fresh',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Discover'),
     );
   }
 }
@@ -35,38 +33,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          widget.title,
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView(children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Icon(Icons.search_outlined),
+                suffixIcon: IconButton(
+                    onPressed: () {}, icon: Icon(MdiIcons.barcodeScan)),
+                fillColor: Colors.white,
+                filled: true),
+          ),
+        ),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined),
+              label: "Discover",
+              activeIcon: Icon(Icons.search)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.storefront_outlined),
+              label: "Store",
+              activeIcon: Icon(Icons.storefront)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outlined),
+              label: "Profile",
+              activeIcon: Icon(Icons.person)),
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
       ),
     );
   }
