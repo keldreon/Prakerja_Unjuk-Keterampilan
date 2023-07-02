52a2279dfbc5aa48bce67718a4a03d7d7536f267
Keldreon <galih91@live.com>
2023-07-02 11:15:19 +0700

    edited store UI

--- lib/components/store.dart
+++ lib/components/store.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:unjuk_keterampilan/components/discoverNav/scan.dart';
 
 class StoreWidget extends StatefulWidget {
   const StoreWidget({super.key});
@@ -12,13 +14,13 @@ class _StoreWidgetState extends State<StoreWidget> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(15.0),
+        const Padding(
+          padding: EdgeInsets.all(15.0),
           child: Card(
             child: ListTile(
               titleAlignment: ListTileTitleAlignment.center,
               leading: FlutterLogo(size: 35.0),
-              title: const Text(
+              title: Text(
                 'Mason, Ohio',
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
@@ -26,6 +28,43 @@ class _StoreWidgetState extends State<StoreWidget> {
               subtitle: Text('5155 Financial Way,\nMason, OH 45040'),
               trailing: Text('Change', style: TextStyle(color: Colors.green)),
               isThreeLine: true,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.all(15),
+                prefixIcon: const Icon(Icons.search_outlined),
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (_) => const Scan()));
+                    },
+                    icon: Icon(MdiIcons.barcodeScan)),
+                fillColor: Colors.white,
+                filled: true),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+          child: const Text('Shop by Category'),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Card(
+            child: ListTile(
+              titleAlignment: ListTileTitleAlignment.center,
+              leading: FlutterLogo(size: 35.0),
+              title: Text(
+                'Produce',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 20),
+              ),
             ),
           ),
         ),
