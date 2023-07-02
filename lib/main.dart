37555dedf222ba2865c230e560a56517d5870c1c
Keldreon <galih91@live.com>
2023-07-02 12:20:42 +0700

    edited systemchrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:unjuk_keterampilan/components/discover.dart';
 
 import 'components/profile.dart';
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       title: 'Shop Fresh',
       debugShowCheckedModeBanner: false,
