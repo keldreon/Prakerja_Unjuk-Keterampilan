87732b7628685bdfb73322adbae651df4b257741
Keldreon <galih91@live.com>
2023-07-03 05:04:30 +0700

    added featured_items and image, completed scan, and edited discover

--- lib/components/discoverNav/featured_items.dart
+++ lib/components/discoverNav/featured_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unjuk_keterampilan/theme/theme.dart';
 
 class FeaturedItems extends StatefulWidget {
   const FeaturedItems({super.key});
@@ -10,6 +11,25 @@ class FeaturedItems extends StatefulWidget {
 class _FeaturedItemsState extends State<FeaturedItems> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        title: 'Featured Items',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.theme,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: IconButton(
+                icon: const Icon(Icons.keyboard_arrow_left_rounded),
+                iconSize: 30,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+          ),
+          body: const Text('Items here'),
+        ));
   }
 }
