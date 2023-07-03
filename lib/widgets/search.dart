43410dded975d0d3367b862fe38e224ba3ee3008
Keldreon <galih91@live.com>
2023-07-03 13:42:05 +0700

    fixed search

--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:unjuk_keterampilan/theme/theme.dart';
 
-import '../discoverNav/scan.dart';
+import 'package:unjuk_keterampilan/components/discoverNav/scan.dart';
 
 class SearchWidget extends StatelessWidget {
   const SearchWidget({
