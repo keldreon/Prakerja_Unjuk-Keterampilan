f0ee08ca741f7bcff9d7d59b72a5707df5fa894b
Keldreon <galih91@live.com>
2023-07-03 07:54:53 +0700

    competed featured_items

--- lib/components/discoverNav/featured_items.dart
+++ lib/components/discoverNav/featured_items.dart
@@ -17,14 +17,13 @@ class _FeaturedItemsState extends State<FeaturedItems> {
         debugShowCheckedModeBanner: false,
         theme: AppTheme.theme,
         home: Scaffold(
-          body: SafeArea(
-            top: false,
-            child: Column(
+          body: ListView(children: [
+            Column(
               children: [
                 Stack(children: [
                   Container(
                     width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 0.4,
+                    height: MediaQuery.of(context).size.height * 0.35,
                     decoration: const BoxDecoration(
                       image: DecorationImage(
                         fit: BoxFit.fitWidth,
@@ -53,90 +52,237 @@ class _FeaturedItemsState extends State<FeaturedItems> {
                 ]),
                 SizedBox(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: ListView(children: [
-                      ListTile(
-                        tileColor: Pallete.whiteColor,
-                        titleAlignment: ListTileTitleAlignment.top,
-                        title: const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Strawberries',
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w800, fontSize: 25),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Pepperidge Farm',
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 12,
-                                  color: Pallete.unselected),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-                              child: Expanded(
-                                flex: 4,
-                                child: Text(
-                                  'The Graden strawberry is a widely grown hybrid species of genus Fragraria, collectively known as the strawberries, which are cultivated worldwide for their fruit.',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w100,
-                                      fontSize: 12,
-                                      color: Pallete.unselected),
-                                ),
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  child: ListView(children: [
+                    ListTile(
+                      tileColor: Pallete.whiteColor,
+                      titleAlignment: ListTileTitleAlignment.top,
+                      title: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Strawberries',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800, fontSize: 25),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Pepperidge Farm',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                fontSize: 12,
+                                color: Pallete.unselected),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                            child: Expanded(
+                              flex: 4,
+                              child: Text(
+                                'The Graden strawberry is a widely grown hybrid species of genus Fragraria, collectively known as the strawberries, which are cultivated worldwide for their fruit.',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w100,
+                                    fontSize: 12,
+                                    color: Pallete.unselected),
                               ),
                             ),
-                          ],
-                        ),
-                        subtitle: const Row(
-                          children: [
-                            Text(
-                              '\$ 2.00',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 12,
-                                  decoration: TextDecoration.lineThrough,
-                                  color: Pallete.unselected),
-                            ),
-                            Text(
-                              '\$ 1.30',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 12,
-                                  color: Pallete.redColor),
-                            ),
-                          ],
-                        ),
-                        trailing: IconButton(
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (_) => const FeaturedItems()));
-                          },
-                          icon: Icon(MdiIcons.plus),
-                          color: Pallete.greenColor,
-                          iconSize: 35,
-                          alignment: Alignment.center,
-                        ),
+                          ),
+                        ],
                       ),
-                    ]),
+                      subtitle: const Row(
+                        children: [
+                          Text(
+                            '\$ 2.00',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                fontSize: 12,
+                                decoration: TextDecoration.lineThrough,
+                                color: Pallete.unselected),
+                          ),
+                          Text(
+                            '\t\$ 1.30',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                fontSize: 12,
+                                color: Pallete.redColor),
+                          ),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) => const FeaturedItems()));
+                        },
+                        icon: Icon(MdiIcons.plus),
+                        color: Pallete.greenColor,
+                        iconSize: 35,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ]),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Row(
+                    children: [
+                      Text('Featured Recipe'),
+                    ],
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 350,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView(
+                      shrinkWrap: true,
+                      physics: const ScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 5, vertical: 5),
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 350,
+                              width: MediaQuery.of(context).size.width,
+                              child: ListView(
+                                  shrinkWrap: true,
+                                  physics: const ScrollPhysics(),
+                                  scrollDirection: Axis.horizontal,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5, vertical: 5),
+                                  children: <Widget>[
+                                    //reciewve detail
+                                    Container(
+                                      padding: const EdgeInsets.all(10),
+                                      width: 200,
+                                      height: 300,
+                                      child: Card(
+                                        surfaceTintColor: Pallete.whiteColor,
+                                        elevation: 10,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        color: Pallete.whiteColor,
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 5.0, vertical: 5.0),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                child: Image.asset(
+                                                  '/img/Image.png',
+                                                  height: 100,
+                                                  fit: BoxFit.contain,
+                                                  width: double.infinity,
+                                                ),
+                                              ),
+                                              const ListTile(
+                                                titleAlignment:
+                                                    ListTileTitleAlignment
+                                                        .center,
+                                                title: SizedBox(
+                                                  width: 50,
+                                                  height: 75,
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Text(
+                                                        'Eggplant Salad',
+                                                        style: TextStyle(
+                                                            fontSize: 17),
+                                                        overflow: TextOverflow
+                                                            .ellipsis,
+                                                        maxLines: 2,
+                                                      ),
+                                                      Text(
+                                                        'Rice, bean, tomato, and cilantro in a shell.',
+                                                        style: TextStyle(
+                                                            fontSize: 12),
+                                                        overflow: TextOverflow
+                                                            .ellipsis,
+                                                        maxLines: 4,
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                                subtitle: Text(
+                                                  '15 Minutes',
+                                                  maxLines: 1,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style: TextStyle(
+                                                      color:
+                                                          Pallete.greenColor),
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                width: 20,
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 15,
+                                                        vertical: 15),
+                                                child: Row(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.end,
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.end,
+                                                  children: [
+                                                    const SizedBox(
+                                                      width: 30,
+                                                    ),
+                                                    Expanded(
+                                                        child: TextButton(
+                                                      style: TextButton.styleFrom(
+                                                          backgroundColor:
+                                                              Pallete
+                                                                  .greenColor),
+                                                      onPressed: () {},
+                                                      child: const Text(
+                                                        'View',
+                                                        style: TextStyle(
+                                                            color: Pallete
+                                                                .whiteColor2,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .w300),
+                                                      ),
+                                                    ))
+                                                  ],
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                          ],
+                        ),
+                        //reciewve detail
+                      ]),
+                ),
               ],
             ),
-          ),
+          ]),
         ));
   }
 }
